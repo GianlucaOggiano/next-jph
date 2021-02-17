2c9704339fe33caeda12113c6b6979ea9a2d3690
Gianluca Oggiano <gianlucao89@gmail.com>
2021-02-17 18:07:55 +0100

    Add bg color change on scroll logic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,36 @@
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { FaGithub } from 'react-icons/fa';
 
 const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  const isScrolled = () => {
+    window.scrollY > 30 ? setScrolled(true) : setScrolled(false);
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', isScrolled);
+    return () => window.removeEventListener('scroll', isScrolled);
+  }, []);
+
   return (
-    <header>
+    <header className={scrolled && 'active'}>
       <nav className='container header__nav'>
         <div className='header__nav-logo'>
           <h3>
             <Link href='/'>Next Jph</Link>
           </h3>
         </div>
-        <ul className='header__nav-items'>icons goes here</ul>
+        <ul className='header__nav-items'>
+          <li>
+            <a
+              href='https://github.com/GianlucaOggiano/next-jph'
+              target='_blank'
+            >
+              <FaGithub />
+            </a>
+          </li>
+        </ul>
       </nav>
     </header>
   );
