6d5e1cc9367fc9d7585db0a11f81720ecb5e506e
Gianluca Oggiano <gianlucao89@gmail.com>
2021-02-17 18:05:15 +0100

    Create home banner component

---
+++ src/components/HomeIntro.js
@@ -0,0 +1,21 @@
+const HomeIntro = () => {
+  return (
+    <div className='home__intro'>
+      <div className='home__intro-left'>
+        <div className='home__intro-left-info'>
+          <h1>Lorem ipsum dolor sit.</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam
+            earum ex quod aperiam perferendis consectetur aut voluptatem at
+            atque iure.
+          </p>
+        </div>
+      </div>
+      <div className='home__intro-right'>
+        <img src='/img/banner.svg' alt='' />
+      </div>
+    </div>
+  );
+};
+
+export default HomeIntro;
